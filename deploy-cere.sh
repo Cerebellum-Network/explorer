e95403f986a64d1f21f86873a10a591a3d60c299
Evgeny Svirsky <evgeny@cere.io>
2021-01-23 15:39:02 +0300

    * Updated script.

--- deploy-cere.sh
+++ deploy-cere.sh
@@ -5,4 +5,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 
-nvm exec yarn build && cd ./packages/apps/ && aws s3 sync build/ s3://block-viewer.cere.io --profile=cerebellum
+
+aws cloudfront create-invalidation --distribution-id E3FW68C232HTDC --paths "/*" --profile=cerebellum
+
+nvm exec yarn build cd ./packages/apps/ && aws s3 sync build/ s3://block-viewer.cere.io --profile=cerebellum
