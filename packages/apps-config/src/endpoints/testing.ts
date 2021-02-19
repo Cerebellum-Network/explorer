c90a32c259f33fb7993c4f2a8dfd154e702f44d4
Evgeny Svirsky <evgeny@cere.io>
2021-02-19 12:05:59 +0300

    * Updated Cere URL

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -32,7 +32,7 @@ export function createTesting (t: TFunction): LinkOption[] {
       info: 'cere',
       text: t('rpc.cere', 'Cerebellum Network', { ns: 'apps-config' }),
       providers: {
-        CerebellumNetwork: 'wss://testnet-node-0.cere.network:9945'
+        CerebellumNetwork: 'wss://testnet-node-1.cere.network:9945'
       }
     },
     {
