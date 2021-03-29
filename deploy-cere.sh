c4dd5410578f697867893e2c0a9a420c17fb8da3
Evgeny Svirsky <evgeny@cere.io>
2021-03-29 18:53:03 +0300

    * Added yarn installation * Install deps via yarn

--- deploy-cere.sh
+++ deploy-cere.sh
@@ -4,7 +4,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+nvm exec npm i yarn -g
 
+nvm exec yarn install
 
 aws cloudfront create-invalidation --distribution-id E3FW68C232HTDC --paths "/*" --profile=cerebellum
 
