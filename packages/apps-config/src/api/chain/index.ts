2675fbce2c32e2d2d48a1cb5ee37bde278676e33
doasync <doasync@gmail.com>
2021-04-28 17:43:26 +0300

    fix lint errors

--- packages/apps-config/src/api/chain/index.ts
+++ packages/apps-config/src/api/chain/index.ts
@@ -1,13 +1,13 @@
 // Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import Phala from './phala';
 import Cere from './cere';
+import Phala from './phala';
 
 // alphabetical, based on the actual displayed name
 export default {
-  'Phala PoC-3': Phala,
   'Cerebellum Network Testnet': Cere,
   'Cerebellum Network Testnet Beta': Cere, // For Substrate cluster
   'Local Testnet': Cere, // For Nodes bootstraped from --local chainspec
+  'Phala PoC-3': Phala
 };
