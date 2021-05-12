0bee7dff1d72484a4d45dacdfbc2f51a8c092b80
Evgeny Svirsky <evgeny@cere.io>
2021-05-12 12:05:44 +0200

    * Updated config for the reverted runtime.

--- packages/apps-config/src/api/chain/cere.ts
+++ packages/apps-config/src/api/chain/cere.ts
@@ -5,23 +5,7 @@
 /* eslint-disable sort-keys */
 
 export default {
-  ChainId: 'u8',
-  DepositNonce: 'u64',
-  ResourceId: '[u8; 32]',
-  ProposalStatus: {
-    _enum: ['Initiated', 'Approved', 'Rejected']
-  },
-  ProposalVotes: {
-    votes_for: 'Vec<AccountId>',
-    votes_against: 'Vec<AccountId>',
-    status: 'ProposalStatus',
-    expiry: 'BlockNumber'
-  },
-  TokenId: 'u256',
-  Erc721Token: {
-    id: 'TokenId',
-    metadata: 'Vec<u8>'
-  },
-  Address: 'AccountId',
-  LookupSource: 'AccountId'
+  ChainId: "u8",
+  ResourceId: "[u8; 32]",
+  TokenId: "U256"
 };
