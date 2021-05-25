3f58324175ebdf99f3e4cade2bb400dcdf8e2709
Karthikeyan <karthikeyan@cere.io>
2021-05-25 15:19:24 +0530

    adding mainnet alpha url

--- packages/apps-config/src/endpoints/testing.ts
+++ packages/apps-config/src/endpoints/testing.ts
@@ -27,7 +27,14 @@ export function createTesting (t: TFunction): LinkOption[] {
     },
     {
       info: 'cere',
-      text: t('rpc.cere', 'Cerebellum Network', { ns: 'apps-config' }),
+      text: t('rpc.cere', 'Cerebellum Mainnet Alpha Stage', { ns: 'apps-config' }),
+      providers: {
+        'Cerebellum Network': 'wss://archive.mainnet.stage.cere.network:9945'
+      }
+    },
+    {
+      info: 'cereTestnet',
+      text: t('rpc.cere', 'Cerebellum Testnet', { ns: 'apps-config' }),
       providers: {
         'Cerebellum Network': 'wss://rpc.testnet.cere.network:9945'
       }
