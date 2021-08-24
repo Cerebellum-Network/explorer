14cbdcdc19408ad6d403e2f3979d2bbee3fc372e
Andrei Navoichyk <andrei@cere.io>
2021-08-24 16:31:05 +0300

    added QAnet support

--- packages/apps-config/src/api/chain/index.ts
+++ packages/apps-config/src/api/chain/index.ts
@@ -2,20 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import Cere from './cere';
-import CereQa from './cereQA';
 import Phala from './phala';
 
 // alphabetical, based on the actual displayed name
 export default {
-  'Cerebellum Devnet': Cere,
   'Cerebellum Ext Devs Network 1': Cere,
   'Cerebellum Ext Devs Network 2': Cere,
   'Cerebellum Ext Devs Network 3': Cere,
   'Cerebellum Ext Devs Network 4': Cere,
   'Cerebellum Ext Devs Network 5': Cere,
-  'Cerebellum Mainnet Alpha': Cere,
-  'Cerebellum Network Testnet': CereQa,
+  'Cerebellum Devnet': Cere,
+  'Cerebellum QAnet': Cere,
   'Cerebellum Testnet': Cere,
+  'Cerebellum Mainnet Alpha': Cere,
   'Local Testnet': Cere, // For Nodes bootstraped from --local chainspec
   'Phala PoC-3': Phala
 };
