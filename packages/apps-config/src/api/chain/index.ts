711f40d15eb8487ab7dd2ff54787786b6c952096
Andrei Navoichyk <andrei@cere.io>
2022-06-30 15:34:54 +0400

    renamed Cere QAnet

--- packages/apps-config/src/api/chain/index.ts
+++ packages/apps-config/src/api/chain/index.ts
@@ -14,7 +14,7 @@ export default {
   'Cere Ext Devs Network 5': Cere,
   'Cere Mainnet Alpha': Cere,
   'Cere Mainnet Beta': Cere,
-  'Cere QAnet': Cere,
+  'Cere Qanet': Cere,
   'Cere Testnet': Cere,
   'Cerebellum Devnet': Cere,
   'Cerebellum Ext Devs Network 1': Cere,
