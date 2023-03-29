9ffa8fa94ce1994923253dac8ac94e8d82941abf
shamilkhan <shamilkhan1999@gmail.com>
2023-03-29 21:33:54 +0400

    fix(*): fix "module": "node16" typescript issue
    https://github.com/microsoft/TypeScript/issues/49083

--- packages/apps/test/endpoint.spec.ts
+++ packages/apps/test/endpoint.spec.ts
@@ -1,7 +1,7 @@
 // Copyright 2017-2021 @polkadot/apps authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { isValidUrl } from '../src/Endpoints/index';
+import { isValidUrl } from '../src/Endpoints/index.js';
 
 describe('Test isValidUrl function', () => {
   test.each([
